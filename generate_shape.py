63d658969ec2ad63d304ede9ad7540162acd3a48
hkeramat <37190613+hkeramat@users.noreply.github.com>
2022-01-18 10:08:32 -0500

    Update generate_shape.py

--- generate_shape.py
+++ generate_shape.py
@@ -1,15 +1,8 @@
-# Custom imports
+
 from shapes_utils import *
 from meshes_utils import *
 
-### ************************************************
-### Main execution
-### Parameters controlling random shape generation and meshing
-### radius         : local radius of curve around control points, in [0,1]   (maximal sharpness  for radius = 1)
-### edgy           : controls the smoothness of the curve, in [0,1] (maximal smoothness for edgy   = 0)
-### n_pts          : number of random points joined together by Bezier curves
-### n_sampling_pts : number of sampled points on each Bezier curve joining two control points
-### plot_pts       : True to plot the position of control points in shape image
+
 n_pts          = 5
 n_sampling_pts = 10
 radius         = 0.5*np.ones([n_pts])
@@ -18,12 +11,14 @@ plot_pts       = True
 filename       = '3359.csv'
 cylinder       = False
 
-# Generate and mesh shape
+
 shape = Shape(filename,None,n_pts,n_sampling_pts,radius,edgy)
-#shape = Shape()
+
 shape.read_csv(filename)
 shape.generate()
-#shape.generate(cylinder=cylinder)
+
+
+
 shape.mesh(mesh_domain = True,
            shape_h     = 1.0,
            domain_h    = 1.0,
